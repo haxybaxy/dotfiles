f69155aa0b3bfe6591b57f00cca960254190eddc
haxy <zaidsaheb4@gmail.com>
2024-11-13 12:32:43 +0100

    added rest of my files

---
+++ dot_wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+
+return {
+    -- Font settings
+    font = wezterm.font("MesloLGS Nerd Font Mono"),
+    -- Window Settings
+    window_background_opacity = 0.8,
+    window_decorations = 'RESIZE',
+    enable_tab_bar = false,
+}
+
