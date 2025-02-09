cc79339862ee511fce035f75b87452a125ddfd28
haxy <zaidsaheb4@gmail.com>
2025-02-09 17:26:01 +0100

    added autocomplete/snippets

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,29 +1,62 @@
 return {
 	{
-	"williamboman/mason.nvim",
+		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 
 	{
-	"williamboman/mason-lspconfig.nvim",
+		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "ts_ls", "tailwindcss", "cssls", "eslint", "graphql", "html", "jsonls" }
+				ensure_installed = { "lua_ls", "ts_ls", "tailwindcss", "cssls", "eslint", "graphql", "html", "jsonls" },
 			})
-		end
+		end,
 	},
 	{
-	"neovim/nvim-lspconfig",
-		config  = function()
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			vim.keymap.set('n','K', vim.lsp.buf.hover, {}) -- show info in a hover
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.graphql.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- show info in a hover
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {}) -- go to definition
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {}) -- go to references
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {}) -- perform code action
-		end
-	}
+		end,
+	},
 }
