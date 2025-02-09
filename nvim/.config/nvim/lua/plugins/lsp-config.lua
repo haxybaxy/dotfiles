9c8472838bd977ebf4fecf115a65e60a0b7f79d3
haxy <zaidsaheb4@gmail.com>
2025-02-09 16:05:22 +0100

    none-ls

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "ts_ls" }
+			ensure_installed = { "lua_ls", "ts_ls", "tailwindcss", "cssls", "eslint", "graphql", "html", "jsonls" }
 			})
 		end
 	},
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
-return { 
+return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
-		config.setup({  
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "json", "python", "javascript", "html", "css" },
-		highlight = { enable = true },
-        	indent = { enable = true },
+		config.setup({
+			auto_install = true,
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "json", "python", "javascript", "html", "css" },
+			highlight = { enable = true },
+			indent = { enable = true },
 		})
-	end
+	end,
 } -- syntax highlighting
