a760aee48d17a1aa9cc892c781b55176d219b5df
haxy <zaidsaheb4@gmail.com>
2025-02-13 14:36:27 +0100

    made it work with django ;D

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,6 +5,21 @@ return {
 	},
 
 	config = function()
+		local function find_django_settings()
+			local handle = io.popen("find . -name settings.py | head -n 1")
+			if handle then
+				local result = handle:read("*a")
+				handle:close()
+				if result and result ~= "" then
+					result = result:gsub("%./", "") -- Remove leading "./"
+					result = result:gsub("/settings.py", "") -- Remove file name
+					result = result.gsub(result, "%s+", "")
+					return result:gsub("/", ".") .. ".settings" -- Convert to module format
+				end
+			end
+			return "main.settings" -- Default fallback
+		end
+
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
@@ -13,10 +28,17 @@ return {
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.completion.spell,
 				require("none-ls.diagnostics.eslint"),
-				null_ls.builtins.diagnostics.pylint,
+				null_ls.builtins.diagnostics.pylint.with({
+					command = "pylint",
+					debounce = 100,
+					extra_args = {
+						"--disable=C0111",
+						"--load-plugins=pylint_django",
+						"--django-settings-module=" .. find_django_settings(),
+					},
+				}),
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
-
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
