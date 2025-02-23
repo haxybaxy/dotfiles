60823afc4f7b58a8dedf8c3e2c11a3e8f2631c42
haxy <zaidsaheb4@gmail.com>
2025-02-23 02:00:44 +0100

    looking good with luabar

---
+++ sketchybar/.config/sketchybar/items/init.lua
@@ -0,0 +1,9 @@
+-- Left items
+require("items.spaces")
+require("items.front_apps")
+
+-- Right items
+require("items.message")
+require("items.widgets")
+
+-- require("items.media")
--- sketchybar/.config/sketchybar/plugins/cpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cpu_usage=$(top -l 1 | awk '/CPU usage/ {print $3}' | sed 's/%//')
-echo $cpu_usage
-
