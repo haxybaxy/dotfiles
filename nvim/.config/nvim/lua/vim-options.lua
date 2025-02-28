c8ebb8ecbb940160a370522f11a35fe3e7ed5001
haxy <zaidsaheb4@gmail.com>
2025-02-28 21:53:02 +0100

    added options for neovide

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -13,3 +13,25 @@ vim.o.undofile = true
 
 
 vim.g.mapleader = " "
+
+
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Disable arrow keys
+vim.api.nvim_set_keymap('n','<Up>','<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n','<Down>','<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n','<Left>','<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n','<Right>','<Nop>', { noremap = true, silent = true})
+
+
+vim.api.nvim_set_keymap('i','<Up>','<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i','<Down>','<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i','<Left>','<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i','<Right>','<Nop>', { noremap = true, silent = true})
