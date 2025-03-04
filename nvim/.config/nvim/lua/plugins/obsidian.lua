ee895dd6356f8426f37a028a925ea8b828590fa6
haxy <zaidsaheb4@gmail.com>
2025-03-04 12:04:41 +0100

    added obsidian

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,17 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "notes",
+        path = "/Users/zaidsaheb/Library/Mobile Documents/iCloud~md~obsidian/Documents/vault16",
+      },
+    },
+  },
+}
