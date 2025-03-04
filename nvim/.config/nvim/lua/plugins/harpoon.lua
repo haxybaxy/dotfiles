b60c2ba7e2ef812e3e49199b2691e2c29d70ad9d
haxy <zaidsaheb4@gmail.com>
2025-03-04 12:32:55 +0100

    proper keybinds

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -18,10 +18,10 @@ return {
 		vim.keymap.set("n", "<leader>w", function()
 			harpoon:list():select(2)
 		end)
-		vim.keymap.set("n", "<leader>e", function()
+		vim.keymap.set("n", "<leader>r", function()
 			harpoon:list():select(3)
 		end)
-		vim.keymap.set("n", "<leader>r", function()
+		vim.keymap.set("n", "<leader>t", function()
 			harpoon:list():select(4)
 		end)
 
