7838d9c7fb5f18273e990fc9b88f04758f5a96f6
haxybaxy <zaidsaheb4@gmail.com>
2025-06-03 01:00:31 +0200

    new changes

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -1,13 +1,13 @@
 return {
---  'romgrk/barbar.nvim',
---  dependencies = {
---    'lewis6991/gitsigns.nvim',       -- OPTIONAL: for git status
---    'nvim-tree/nvim-web-devicons',   -- OPTIONAL: for file icons
---  },
---  config = function()
---    vim.g.barbar_auto_setup = false
---    require("barbar").setup({
---      auto_hide = true
---    })
---  end
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim',       -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons',   -- OPTIONAL: for file icons
+  },
+  config = function()
+    vim.g.barbar_auto_setup = false
+    require("barbar").setup({
+      auto_hide = 3
+    })
+  end
 }
