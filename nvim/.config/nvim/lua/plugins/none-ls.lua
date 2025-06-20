8ef779ceb456a92daa1a3b885fda72c3017c0d07
haxybaxy <zaidsaheb4@gmail.com>
2025-06-20 23:53:23 +0200

    removed eslint diagnostics

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -23,7 +23,6 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
         null_ls.builtins.completion.spell,
-        require("none-ls.diagnostics.eslint"),
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
