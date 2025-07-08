1f3831608832f6167bd46ea01a52176514d9eff8
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-07-08 16:09:37 +0200

    added csv viewer

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,25 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "json", "python", "javascript", "html", "css" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "json",
+        "python",
+        "javascript",
+        "html",
+        "css",
+        "csv",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 } -- syntax highlighting
