e4cd65aa868f4e2f55476a9a24413b3f27106664
haxybaxy <zaidsaheb4@gmail.com>
2025-07-10 23:51:16 +0200

    clean up snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -29,42 +29,17 @@ return {
     -- },
   },
   keys = {
-    -- Top Pickers & Explorer
-    { "<leader>n",  function() Snacks.picker.notifications() end,    desc = "Notification History" },
     -- { "<leader>e",  function() Snacks.explorer() end,                desc = "File Explorer" },
-    -- find
-    { "<leader>z",  function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
-    { "<leader>Z",  function() Snacks.zen.zoom() end,                desc = "Toggle Zoom" },
+    { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
+
+    { "<leader>lg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
+
+    { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
+    { "<leader>n",  function() Snacks.notifier.show_history() end,   desc = "Notification History" },
+
     { "<leader>.",  function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
     { "<leader>S",  function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
-    { "<leader>n",  function() Snacks.notifier.show_history() end,   desc = "Notification History" },
-    { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
-    { "<leader>cR", function() Snacks.rename.rename_file() end,      desc = "Rename File" },
-    { "<leader>gB", function() Snacks.gitbrowse() end,               desc = "Git Browse",               mode = { "n", "v" } },
-    { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
-    { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
-    { "<leader>t",  function() Snacks.terminal() end,                desc = "Toggle Terminal" },
-    { "<c-_>",      function() Snacks.terminal() end,                desc = "which_key_ignore" },
-    { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",           mode = { "n", "t" } },
-    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",           mode = { "n", "t" } },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        Snacks.win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
-    },
+
     config = function()
       vim.api.nvim_create_autocmd("User", {
         pattern = "VeryLazy",
