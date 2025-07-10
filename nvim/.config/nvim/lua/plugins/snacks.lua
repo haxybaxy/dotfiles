6b71fc1a96392a46a65c8a446f59b9c5ef2a4106
haxybaxy <zaidsaheb4@gmail.com>
2025-07-11 00:10:20 +0200

    lint

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,107 +1,106 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    animate = { enabled = true },
-    bigfile = { enabled = true },
-    quickfle = { enabled = true },
-    explorer = { enabled = true },
-    terminal = { enabled = true },
-    image = { enabled = true },
-    notifier = {
-      enabled = true,
-      timeout = 3000,
-    },
-    -- picker = {
-    --   sources = {
-    --     files = { hidden = true },
-    --     explorer = {
-    --       include = { ".*", "hidden" },
-    --       layout = {
-    --         layout = {
-    --           position = "right",
-    --         },
-    --       },
-    --     },
-    --   },
-    -- },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		animate = { enabled = true },
+		bigfile = { enabled = true },
+		quickfle = { enabled = true },
+		explorer = { enabled = true },
+		terminal = { enabled = true },
+		image = { enabled = true },
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
+		-- picker = {
+		--   sources = {
+		--     files = { hidden = true },
+		--     explorer = {
+		--       include = { ".*", "hidden" },
+		--       layout = {
+		--         layout = {
+		--           position = "right",
+		--         },
+		--       },
+		--     },
+		--   },
+		-- },
 
-  styles = {
+		styles = {
 
-    lazygit = {
-      height = 0.9,
-      width = 0.9,
-      backdrop = 100,
-    },
-  },
-  },
+			lazygit = {
+				height = 0.9,
+				width = 0.9,
+				backdrop = 100,
+			},
+		},
+	},
 
+	keys = {
+		-- { "<leader>e",  function() Snacks.explorer() end,                desc = "File Explorer" },
+		{
+			"<leader>bd",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete Buffer",
+		},
 
-  keys = {
-    -- { "<leader>e",  function() Snacks.explorer() end,                desc = "File Explorer" },
-    {
-      "<leader>bd",
-      function()
-        Snacks.bufdelete()
-      end,
-      desc = "Delete Buffer",
-    },
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
 
-    {
-      "<leader>lg",
-      function()
-        Snacks.lazygit()
-      end,
-      desc = "Lazygit",
-    },
+		{
+			"<leader>un",
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = "Dismiss All Notifications",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
 
-    {
-      "<leader>un",
-      function()
-        Snacks.notifier.hide()
-      end,
-      desc = "Dismiss All Notifications",
-    },
-    {
-      "<leader>n",
-      function()
-        Snacks.notifier.show_history()
-      end,
-      desc = "Notification History",
-    },
+		{
+			"<leader>.",
+			function()
+				Snacks.scratch()
+			end,
+			desc = "Toggle Scratch Buffer",
+		},
+		{
+			"<leader>S",
+			function()
+				Snacks.scratch.select()
+			end,
+			desc = "Select Scratch Buffer",
+		},
 
-    {
-      "<leader>.",
-      function()
-        Snacks.scratch()
-      end,
-      desc = "Toggle Scratch Buffer",
-    },
-    {
-      "<leader>S",
-      function()
-        Snacks.scratch.select()
-      end,
-      desc = "Select Scratch Buffer",
-    },
+		config = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					_G.bt = function()
+						Snacks.debug.backtrace()
+					end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-    config = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-          -- Create some toggle mappings
-        end,
-      })
-    end,
-  },
+					-- Create some toggle mappings
+				end,
+			})
+		end,
+	},
 }
