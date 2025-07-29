65033c9b73b26f4d1f6467d798347c8694877222
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-07-29 10:14:25 +0200

    atom one light instead of github light

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,28 +1,33 @@
 return {
-	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme github_dark_default")
-		end,
-	},
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 999,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme github_dark_default")
+    end,
+  },
 
-	{
-		"f-person/auto-dark-mode.nvim",
-		config = function()
-			require("auto-dark-mode").setup({
-				update_interval = 1000,
-				set_dark_mode = function()
-					vim.o.background = "dark"
-					vim.cmd("colorscheme github_dark_default")
-				end,
-				set_light_mode = function()
-					vim.o.background = "light"
-					vim.cmd("colorscheme github_light_default")
-				end,
-			})
-		end,
-	},
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = function()
+      require("auto-dark-mode").setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.o.background = "dark"
+          vim.cmd("colorscheme github_dark_default")
+        end,
+        set_light_mode = function()
+          vim.o.background = "light"
+          vim.cmd("colorscheme onedark")
+        end,
+      })
+    end,
+  },
 }
