c595a308c493728bd8412d62d218f71b3391c0cd
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-07-29 13:03:21 +0200

    better copy

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -37,7 +37,7 @@ end
 -- vim.api.nvim_set_keymap('i','<Right>','<Nop>', { noremap = true, silent = true})
 
 -- Yank to system clipboard in normal mode with <leader>y
-vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
 
 -- Yank to system clipboard in visual mode with <leader>y
