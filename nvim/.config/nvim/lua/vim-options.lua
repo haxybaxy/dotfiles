071693cb9d5b31dc5ab4e9c1349ae4f9e2f83369
haxybaxy <zaidsaheb4@gmail.com>
2025-07-30 20:59:12 +0200

    better options

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,23 +1,24 @@
+vim.g.mapleader = " " -- Set leader key to space
+
 vim.cmd("set relativenumber") -- relative numbers for the rest
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab") -- spaces instead of tabs
+vim.cmd("set tabstop=2") -- 2 spaces for a tab
+vim.cmd("set softtabstop=2") -- 2 spaces for soft tab
+vim.cmd("set shiftwidth=2") -- 2 spaces for autoindent
 
-vim.o.undofile = true
+vim.o.undofile = true -- Enable persistent undo
 
-vim.o.wrap = false
-
-vim.g.mapleader = " "
-
+vim.o.wrap = false -- Disable line wrapping
 
 -- Yank to system clipboard in normal and visual mode with <leader>y
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+
+-- Paste from system clipboard in normal mode and visual mode with <leader>p
+vim.keymap.set({ 'n', 'v', 'x' }, "<leader>p", '"+p', { noremap = true, silent = true })
 
 
-
+-- neovide gui settings with macOS keybinds
 if vim.g.neovide then
   vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
   vim.keymap.set("v", "<D-c>", '"+y') -- Copy
