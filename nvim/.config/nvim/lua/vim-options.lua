10f52f1f809ca5fb04fe6ae39dbabcda5f24925e
haxybaxy <zaidsaheb4@gmail.com>
2025-07-30 20:50:43 +0200

    tidy options

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,13 +6,17 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
---vim.opt.clipboard = 'unnamedplus'
-
 vim.o.undofile = true
 
-
+vim.o.wrap = false
 
 vim.g.mapleader = " "
+
+
+-- Yank to system clipboard in normal and visual mode with <leader>y
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+
 
 
 if vim.g.neovide then
@@ -24,22 +28,4 @@ if vim.g.neovide then
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 
--- Disable arrow keys
--- vim.api.nvim_set_keymap('n','<Up>','<Nop>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n','<Down>','<Nop>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n','<Left>','<Nop>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n','<Right>','<Nop>', { noremap = true, silent = true})
--- 
--- 
--- vim.api.nvim_set_keymap('i','<Up>','<Nop>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('i','<Down>','<Nop>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('i','<Left>','<Nop>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('i','<Right>','<Nop>', { noremap = true, silent = true})
-
--- Yank to system clipboard in normal mode with <leader>y
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
-
--- Yank to system clipboard in visual mode with <leader>y
-vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
 
