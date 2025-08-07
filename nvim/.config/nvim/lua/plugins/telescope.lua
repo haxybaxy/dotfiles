a808af1eec17eeaab3e3b8e2aba9dc74072a7678
haxybaxy <zaidsaheb4@gmail.com>
2025-08-08 01:12:52 +0200

    added hidden files to telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
+  { "nvim-telescope/telescope-ui-select.nvim", },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
