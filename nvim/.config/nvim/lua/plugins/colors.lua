167d7c650595173f18832bb120ceed6675c916d6
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-08-08 13:58:49 +0200

    add transparent and ending tildes

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -3,6 +3,12 @@ return {
 		"navarasu/onedark.nvim",
 		lazy = false,
 		priority = 999,
+    config = function()
+      require("onedark").setup({
+        transparent = true,
+        ending_tildes = true,
+      })
+    end
 	},
 
 	{
