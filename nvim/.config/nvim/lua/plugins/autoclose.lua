46da17d068ddc96c03230812a4f8003c7d918373
haxybaxy <zaidsaheb4@gmail.com>
2025-08-09 16:30:16 +0200

    removed comments

--- nvim/.config/nvim/lua/plugins/autoclose.lua
+++ nvim/.config/nvim/lua/plugins/autoclose.lua
@@ -3,18 +3,15 @@ return {
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equivalent to setup({}) function
 	},
 	{
 		"windwp/nvim-ts-autotag",
 		config = function()
 			require("nvim-ts-autotag").setup({
 				opts = {
-					-- Defaults
-					enable_close = true, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
-					enable_close_on_slash = false, -- Auto close on trailing </
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = false,
 				},
 			})
 		end,
