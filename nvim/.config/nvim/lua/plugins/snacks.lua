6613db344e5b1580ec3dc21be21694fa66871953
haxybaxy <zaidsaheb4@gmail.com>
2025-08-09 20:47:41 +0200

    removed pointless debug config

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -44,30 +44,15 @@ return {
     { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
     { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+
     { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer", },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer", },
     { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit", },
+
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications", },
     { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History", },
+
     { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer", },
     { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer", },
-
-    config = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-          -- Create some toggle mappings
-        end,
-      })
-    end,
   },
 }
