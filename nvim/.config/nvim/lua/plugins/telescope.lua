702f5c021152ef8f69f6b361f0ccf4307acae564
haxybaxy <zaidsaheb4@gmail.com>
2025-08-09 16:29:29 +0200

    remove ui select

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvim-telescope/telescope-ui-select.nvim", },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
@@ -11,19 +10,12 @@ return {
             hidden = true,
           },
         },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-
-      require("telescope").load_extension("ui-select")
     end,
   },
 }
