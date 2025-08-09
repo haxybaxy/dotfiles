6f2b566c9d89047365ff9bca482e4a8ad4f29a09
haxybaxy <zaidsaheb4@gmail.com>
2025-08-09 20:43:43 +0200

    moved bunch of stuff inside the coding file

---
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,52 @@
+return {
+	{ -- manipulate surrounding characters
+		"tpope/vim-surround",
+	},
+
+	{ -- better commenting with treesitter
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+	},
+
+	{ -- auto closing brackets
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+
+	{ -- auto close html tags
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = false,
+				},
+			})
+		end,
+	},
+
+	{ -- better % command and highlight matching tag
+		"andymass/vim-matchup",
+		opts = {
+			treesitter = {
+				stopline = 500,
+			},
+		},
+	},
+
+	{ -- render whitespace characters in visual mode
+		"mcauley-penney/visual-whitespace.nvim",
+		config = true,
+		event = "ModeChanged *:[vV\22]",
+		opts = {},
+	},
+
+	{ -- wakatime integration
+		"wakatime/vim-wakatime",
+		lazy = false,
+	},
+}
