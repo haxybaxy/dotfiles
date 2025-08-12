3b4cbf736dbceb9b973b96bd14d0fde55685371c
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-08-12 13:42:46 +0200

    hidden files oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -3,11 +3,11 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+    view_options = {
+      show_hidden = true,
+    },
   },
-  -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
