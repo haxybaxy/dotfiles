472bc7584a9ac4d018d5853a40957f32df307d54
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-08-12 15:30:24 +0200

    new fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,4 +27,14 @@ if status is-interactive
   #starship prompt
   starship init fish | source
 
+  #fish prompt cursor style
+  set fish_cursor_default line
+  set fish_cursor_insert line
+  set fish_cursor_replace_one underscore
+  set fish_cursor_visual block
+
+  # disable fish_greeting
+  function fish_greeting
+  end
+
 end
