4d210b44805b77cb867e8072556d3948e81461c4
haxybaxy <zaidsaheb4@gmail.com>
2025-08-16 16:18:46 +0200

    lint

--- sketchybar/.config/sketchybar/items/widgets/calendar.lua
+++ sketchybar/.config/sketchybar/items/widgets/calendar.lua
@@ -3,29 +3,29 @@ local settings = require("config.settings")
 local icons = require("config.icons")
 
 local function get_clock_icon()
-	local hour = tonumber(os.date("%H"))
-	local hour_12 = hour % 12
-	if hour_12 == 0 then
-		hour_12 = 12
-	end
+  local hour = tonumber(os.date("%H"))
+  local hour_12 = hour % 12
+  if hour_12 == 0 then
+    hour_12 = 12
+  end
 
-	return icons.text.clock["_" .. hour_12] or "?"
+  return icons.text.clock["_" .. hour_12] or "?"
 end
 
 local calendar = sbar.add("item", constants.items.CALENDAR, {
-	position = "right",
-	update_freq = 1,
-	icon = { string = get_clock_icon() },
-	label = { padding_left = 0 },
+  position = "right",
+  update_freq = 1,
+  icon = { string = get_clock_icon() },
+  label = { padding_left = 0 },
 })
 
 calendar:subscribe({ "forced", "routine", "system_woke" }, function(env)
-	calendar:set({
-		icon = { string = get_clock_icon() },
-		label = { string = os.date("%H:%M") },
-	})
+  calendar:set({
+    icon = { string = get_clock_icon() },
+    label = { string = os.date("%H:%M") },
+  })
 end)
 
 calendar:subscribe("mouse.clicked", function(env)
-	sbar.exec("open -a 'Calendar'")
+  sbar.exec("open -a 'Calendar'")
 end)
