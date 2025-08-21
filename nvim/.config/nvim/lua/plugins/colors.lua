6a1997197083f45b40bc1d93f971e77988c76598
haxybaxy <zaidsaheb4@gmail.com>
2025-08-21 23:31:40 +0200

    back to the og

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -8,28 +8,7 @@ return {
         transparent = true,
         ending_tildes = true,
       })
-    end,
-  },
-
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        transparent = true,
-        theme = "wave",
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none",
-              },
-            },
-          },
-        },
-      })
-      vim.cmd("colorscheme kanagawa")
+    vim.cmd("colorscheme onedark")
     end,
   },
 }
