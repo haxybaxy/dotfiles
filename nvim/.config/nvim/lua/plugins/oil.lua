eac5ffe4bee7f3570bcfad65b7365a8d53fb5bea
haxybaxy <zaidsaheb4@gmail.com>
2025-09-06 15:45:51 +0200

    remove annotation

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,5 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
   opts = {
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
     view_options = {
