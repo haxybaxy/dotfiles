99e3243c6fe23cbd2c83d602a50a210bf1dc6039
haxybaxy <zaidsaheb4@gmail.com>
2025-09-09 18:13:30 +0200

    remove widget if there is no meida

--- sketchybar/.config/sketchybar/scripts/media-stream.sh
+++ sketchybar/.config/sketchybar/scripts/media-stream.sh
@@ -7,20 +7,30 @@ media-control stream | \
         # Get the diff flag
         diff=$(jq -r '.diff' <<< "$line")
         
-        new_title=$(jq -r 'if .payload.title then .payload.title else empty end' <<< "$line")
-        new_artist=$(jq -r 'if .payload.artist then .payload.artist else empty end' <<< "$line")
-        new_playing=$(jq -r 'if .payload.playing != null then .payload.playing else empty end' <<< "$line")
+        payload_empty=$(jq -r 'if (.payload | length) == 0 then "true" else "false" end' <<< "$line")
         
-        if [ -n "$new_title" ] && [ "$new_title" != "null" ]; then
-            title="$new_title"
+        if [ "$payload_empty" = "true" ]; then
+            title=""
+            artist=""
+            playing="false"
+        else
+
+            new_title=$(jq -r 'if .payload.title then .payload.title else empty end' <<< "$line")
+            new_artist=$(jq -r 'if .payload.artist then .payload.artist else empty end' <<< "$line")
+            new_playing=$(jq -r 'if .payload.playing != null then .payload.playing else empty end' <<< "$line")
+            
+            if [ -n "$new_title" ] && [ "$new_title" != "null" ]; then
+                title="$new_title"
+            fi
+            if [ -n "$new_artist" ] && [ "$new_artist" != "null" ]; then
+                artist="$new_artist"
+            fi
+            if [ -n "$new_playing" ] && [ "$new_playing" != "null" ]; then
+                playing="$new_playing"
+            fi
         fi
-        if [ -n "$new_artist" ] && [ "$new_artist" != "null" ]; then
-            artist="$new_artist"
-        fi
-        if [ -n "$new_playing" ] && [ "$new_playing" != "null" ]; then
-            playing="$new_playing"
-        fi
-        
+
+        echo "title: $title, artist: $artist, playing: $playing"
         sketchybar --trigger media_stream_changed title="$title" artist="$artist" playing="$playing"
     done
 
