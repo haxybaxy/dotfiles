7f327ec1aa78e55c121884d77ba57613b33c6d1e
haxybaxy <zaidsaheb4@gmail.com>
2025-09-11 17:01:33 +0200

    lint

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,17 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				always_divide_middle = false,
-			},
-			sections = {
-				lualine_x = { "filetype" },
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = 'auto',
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        always_divide_middle = false,
+      },
+      sections = {
+        lualine_x = { "filetype" },
+      },
+    })
+  end,
 }
