5137e84a8aa80e1f023a052b21bab4bc0a243461
haxybaxy <zaidsaheb4@gmail.com>
2025-09-13 15:30:14 +0200

    split up snacks picker

---
+++ nvim/.config/nvim/lua/plugins/snacks_picker/sources.lua
@@ -0,0 +1,48 @@
+local utils = require("plugins.snacks_picker.utils")
+
+local M = {}
+
+M.sources = {
+	select = {
+		layout = { preset = "flow" },
+	},
+
+	files = {
+		hidden = true,
+		ignored = true,
+		layout = function()
+			return utils.get_window_relative_flow_config()
+		end,
+	},
+
+	grep = {
+		layout = function()
+			return utils.get_window_relative_flow_config()
+		end,
+	},
+
+	recent = {
+		layout = function()
+			return utils.get_window_relative_flow_config()
+		end,
+	},
+
+	buffers = {
+		layout = function()
+			return utils.get_window_relative_flow_config()
+		end,
+	},
+
+	explorer = {
+		include = { ".*", "hidden", "ignored" },
+		layout = {
+			layout = {
+				position = "right",
+			},
+		},
+	},
+}
+
+return M
+
+
