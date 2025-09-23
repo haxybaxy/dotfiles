f2a4a2196f6576cdd26797f22723be8a2c182635
haxybaxy <zaidsaheb4@gmail.com>
2025-09-23 19:45:24 +0200

    add macro status

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,14 +4,19 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = 'auto',
+        theme = "auto",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         always_divide_middle = false,
       },
       sections = {
-        lualine_b = {"diagnostics"},
-        lualine_x = {},
+        lualine_b = { "diagnostics" },
+        lualine_x = {
+          {
+            require("noice").api.status.mode.get,
+            cond = require("noice").api.status.mode.has,
+          },
+        },
         lualine_y = { "branch" },
         lualine_z = { "progress" },
       },
