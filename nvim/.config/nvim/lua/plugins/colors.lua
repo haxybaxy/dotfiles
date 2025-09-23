0abd99add87564bee0c040d9aec49971714aa4c0
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-09-23 15:06:06 +0200

    fix catpuccin float windows

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -3,5 +3,17 @@ return {
 		"Mofiqul/vscode.nvim",
 		priority = 1000,
 	},
-	{ "catppuccin/nvim", lazy = false, priority = 999 },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 999,
+		config = function()
+			require("catppuccin").setup({
+				float = {
+					transparent = true,
+					solid = false,
+				},
+			})
+		end,
+	},
 }
