ff1cf66863211b6e1e4b57577dd2d631fd8b017d
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-09-23 16:20:20 +0200

    try to do noice

--- nvim/.config/nvim/lua/plugins/vfx.lua
+++ nvim/.config/nvim/lua/plugins/vfx.lua
@@ -4,18 +4,42 @@ return {
     opts = {},
   },
   {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("noice").setup({
+        cmdline_popup = {
+          views = {
+            cmdline_popup = {
+              border = {
+                style = "none",
+                padding = { 2, 3 },
+              },
+              filter_options = {},
+              win_options = {
+                winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+              },
+            },
+          },
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
   },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-    }
-}
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }
