8922a9f204a70569d0e9c7cd22e37dfb395c82bf
haxybaxy <zaidsaheb4@gmail.com>
2025-09-29 01:34:49 +0200

    revert to old colorscheme logic

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -2,11 +2,6 @@ return {
 	{
 		"Mofiqul/vscode.nvim",
 		priority = 1000,
-		config = function()
-			if vim.o.background == "dark" then
-				vim.cmd("colorscheme vscode")
-			end
-		end,
 	},
 	{
 		"catppuccin/nvim",
@@ -19,9 +14,6 @@ return {
 					solid = false,
 				},
 			})
-			if vim.o.background == "light" then
-				vim.cmd("colorscheme catppuccin")
-			end
 		end,
 	},
 }
