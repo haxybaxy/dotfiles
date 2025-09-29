851e71a13c998f8711eb28533fc9d94aeca9eef5
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-09-29 12:37:24 +0200

    update font

--- sketchybar/.config/sketchybar/config/fonts.lua
+++ sketchybar/.config/sketchybar/config/fonts.lua
@@ -1,8 +1,8 @@
 local dimens = require("config.dimens")
 
 return {
-  text = "Iosevka Nerd Font",
-  numbers = "Iosevka Nerd Font",
+  text = "IosevkaTerm Nerd Font",
+  numbers = "IosevkaTerm Nerd Font",
   icons = function(size)
     local font = "sketchybar-app-font:Regular"
     return size and font .. ":" .. size or font .. ":" .. dimens.text.icon
