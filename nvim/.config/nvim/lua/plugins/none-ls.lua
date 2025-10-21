f61f0c8d7279c8873ff1d74c9e7c6d50d84827bb
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-10-21 14:28:01 +0200

    format on save block added

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,29 +1,44 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-    "jay-babu/mason-null-ls.nvim",
-  },
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+		"jay-babu/mason-null-ls.nvim",
+	},
 
-  config = function()
-    local null_ls = require("null-ls")
-    require("mason-null-ls").setup({
-      ensure_installed = {
-        "stylua",
-        "prettier",
-        "ruff",
-      },
-      automatic_installation = true,
-    })
+	config = function()
+		local null_ls = require("null-ls")
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"ruff",
+			},
+			automatic_installation = true,
+		})
 
-    null_ls.setup({
-      sources = {
-        require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
-        require("none-ls.formatting.ruff_format"),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code with LSP" })
-  end,
+		null_ls.setup({
+			sources = {
+				require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
+				require("none-ls.formatting.ruff_format"),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
+			},
+			-- on_attach = function(client, bufnr) -- Add formatting on save
+			--   -- Enable format on save if the LSP supports it
+			--   if client.supports_method("textDocument/formatting") then
+			--     local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+			--     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			--     vim.api.nvim_create_autocmd("BufWritePre", {
+			--       group = augroup,
+			--       buffer = bufnr,
+			--       callback = function()
+			--         vim.lsp.buf.format({ bufnr = bufnr })
+			--       end,
+			--     })
+			--   end
+			-- end,
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code with LSP" })
+	end,
 }
