629910c45d3dd3be1447463c8cc8be8a3e8061c0
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-10-21 15:57:57 +0200

    disable omni sql default maps

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -1,16 +1,18 @@
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  dependencies = {
-    { "tpope/vim-dadbod",                     lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-  },
-  cmd = {
-    "DBUI",
-    "DBUIToggle",
-    "DBUIAddConnection",
-    "DBUIFindBuffer",
-  },
-  init = function()
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.omni_sql_no_default_maps = 1
+
+	end,
 }
