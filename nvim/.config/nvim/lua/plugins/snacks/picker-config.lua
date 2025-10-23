fc556839091606a7fbb252763c865d7b2198df5f
haxybaxy <zaidsaheb4@gmail.com>
2025-10-23 20:06:07 +0200

    rename again

--- nvim/.config/nvim/lua/plugins/snacks/picker-config.lua
+++ nvim/.config/nvim/lua/plugins/snacks/picker-config.lua
@@ -1,5 +1,5 @@
-local layouts = require("plugins.snacks.snacks-picker.layouts").layouts
-local sources = require("plugins.snacks.snacks-picker.sources").sources
+local layouts = require("plugins.snacks.picker.layouts").layouts
+local sources = require("plugins.snacks.picker.sources").sources
 
 local M = {}
 
