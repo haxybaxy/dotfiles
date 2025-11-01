c5e60110df98dcfb966d41d3ffcecb145601458a
haxybaxy <zaidsaheb4@gmail.com>
2025-11-01 16:08:36 +0100

    add multiple themes

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,9 +1,34 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-      light_style = "day",
-    })
-    vim.cmd("colorscheme tokyonight") end, }
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        light_style = "day",
+      })
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "auto",
+        dark_variant = "moon",
+      })
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_foreground = "default"
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_enable_bold = true
+      vim.g.gruvbox_material_enable_italic = true
+    end,
+  },
+}
