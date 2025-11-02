592981a8366f63f2e003562c673d8f4db8b133f8
haxybaxy <zaidsaheb4@gmail.com>
2025-11-02 22:06:54 +0100

    add custom affirmations

--- nvim/.config/nvim/lua/plugins/snacks/dashboard-config.lua
+++ nvim/.config/nvim/lua/plugins/snacks/dashboard-config.lua
@@ -1,17 +1,20 @@
 local M = {}
 
 M.dashboard = {
-	enabled = true,
-	preset = {
-		header = [[ hang in there! ]],
-	},
-	sections = {
-		{ section = "header" },
-		{ icon = "", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-		{ icon = "", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-		{ icon = "", title = "Projects", section = "projects", indent = 2, padding = 1 },
-		{ section = "startup" },
-	},
+  enabled = true,
+  sections = {
+    {
+      section = "terminal",
+      cmd = "curl -s https://www.affirmations.dev/ | jq -r '.affirmation'",
+      hl = "header",
+      height = 4,
+      align = "center",
+    },
+    { icon = "", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+    { icon = "", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { icon = "", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    { section = "startup" },
+  },
 }
 
 return M
