1199ae7fe6f7371e92d0d96703462347f2c78b33
haxybaxy <zaidsaheb4@gmail.com>
2025-11-03 12:54:00 +0100

    new syntax for diagnostics

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -38,12 +38,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- nicer diagnostic indicators
-local signs = {
-  Error = "●",
-  Warn = "●",
-  Hint = "●",
-  Info = "●"
-}
+vim.diagnostic.config({
+  signs = {
+    -- define text icons per severity
+    text = {
+      [vim.diagnostic.severity.ERROR] = "●",
+      [vim.diagnostic.severity.WARN] = "●",
+      [vim.diagnostic.severity.HINT] = "●",
+      [vim.diagnostic.severity.INFO] = "●",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
+    },
+  },
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+})
 
 -- hide all separators
 vim.opt.fillchars = {
@@ -55,12 +69,6 @@ vim.opt.fillchars = {
   horizdown = " ", -- for bottom separators
   eob = " ",       -- hide end-of-buffer tildes
 }
-
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
 --Keybinds should go under here
 --Tab keybinds
 vim.keymap.set("n", "<leader>tn", "<Cmd>tabnew<CR>", { silent = true, desc = "New Tab"})
