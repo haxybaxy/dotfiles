88db5a4c57d34f8b55df53bc1b4e264bf285c7fc
haxybaxy <zaidsaheb4@gmail.com>
2025-11-03 20:58:16 +0100

    trying more color themes

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,13 +1,21 @@
-return
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        style = "night",
-        light_style = "day",
-        transparent = true,
-      })
-      vim.cmd("colorscheme tokyonight-night")
-    end,
-  }
+return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				light_style = "day",
+				transparent = true,
+			})
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
+	{
+		-- lua/plugins/rose-pine.lua
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+		end,
+	},
+}
