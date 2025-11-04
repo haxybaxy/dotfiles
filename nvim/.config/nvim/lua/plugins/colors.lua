6b29e2a9f406f8a6b5f14d85eb2ff41e883136d3
haxybaxy <zaidsaheb4@gmail.com>
2025-11-04 20:56:30 +0100

    add oasis

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,14 +1,25 @@
 return {
   {
+    "uhs-robert/oasis.nvim",
+    lazy = false,
+    priority = 1000,
+  --   config = function()
+  --     require("oasis").setup({
+  --       dark_style = "night", -- Optional: Choose any style like `lagoon` or 'dune'.
+  --       light_style = "dawn"
+  --     })
+  --   end,
+  },
+  {
     "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
       require("tokyonight").setup({
         style = "night",
-        light_style = "day",
-        transparent = true,
+        light_style = "day"
       })
       vim.cmd("colorscheme tokyonight")
     end,
-  },
+  }
 }
