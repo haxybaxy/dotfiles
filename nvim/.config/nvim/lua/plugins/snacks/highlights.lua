82dafd7c6b09372553938034563771c84d2ef16f
haxybaxy <zaidsaheb4@gmail.com>
2025-11-05 03:24:45 +0100

    format

--- nvim/.config/nvim/lua/plugins/snacks/highlights.lua
+++ nvim/.config/nvim/lua/plugins/snacks/highlights.lua
@@ -2,24 +2,23 @@ local M = {}
 
 -- Apply custom highlight groups for Snacks components
 function M.apply_snacks_highlights()
-  local highlights = {
-    "SnacksPicker",
-    "SnacksPickerPickWin",
-    "SnacksPickerPickWinCurrent",
-    "SnacksPickerBorder",
-  }
-  
-  for _, hl_group in ipairs(highlights) do
-    vim.api.nvim_set_hl(0, hl_group, { link = "StatusLine" })
-  end
+	local highlights = {
+		"SnacksPicker",
+		"SnacksPickerPickWin",
+		"SnacksPickerPickWinCurrent",
+		"SnacksPickerBorder",
+	}
+	for _, hl_group in ipairs(highlights) do
+		vim.api.nvim_set_hl(0, hl_group, { link = "StatusLine" })
+	end
 end
 
 -- Set up autocommand to apply highlights after colorscheme changes
 function M.setup_highlight_autocmd()
-  vim.api.nvim_create_autocmd("ColorScheme", {
-    group = vim.api.nvim_create_augroup("SnacksHighlights", { clear = true }),
-    callback = M.apply_snacks_highlights,
-  })
+	vim.api.nvim_create_autocmd("ColorScheme", {
+		group = vim.api.nvim_create_augroup("SnacksHighlights", { clear = true }),
+		callback = M.apply_snacks_highlights,
+	})
 end
 
 return M
