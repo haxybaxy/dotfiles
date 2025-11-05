9052de902e24b5d606e193dc71c003f83527400a
Zaid Alsaheb <zaidsaheb4@gmail.com>
2025-11-05 15:38:49 +0100

    initial zen

---
+++ nvim/.config/nvim/lua/plugins/snacks/zen-config.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.zen = {
+	enabled = true,
+	toggles = { dim = false, git_signs = false, mini_diff_signs = false },
+	win = {
+		backdrop = { transparent = false, blend = 99 },
+	},
+}
+
+return M
