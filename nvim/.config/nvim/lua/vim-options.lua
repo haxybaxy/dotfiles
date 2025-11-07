00e2c53cfed45e9d04eec85eaeb5f9cfac74a186
haxybaxy <zaidsaheb4@gmail.com>
2025-11-07 01:46:43 +0100

    add autocmd for highlight

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -30,13 +30,25 @@ vim.o.splitright = true -- vsplit to the right
 vim.opt.path:append("**")
 
 -- Ignore common junk directories when searching
-vim.opt.wildignore:append { "*/.git/*", "*/node_modules/*", "*/dist/*", "*/build/*", "*/venv/*", "*/__pycache__/*", "*.egg-info/*", "*/.next/*", "*/ios/*", "*/android/*", "*/.expo/*"}
+vim.opt.wildignore:append({
+  "*/.git/*",
+  "*/node_modules/*",
+  "*/dist/*",
+  "*/build/*",
+  "*/venv/*",
+  "*/__pycache__/*",
+  "*.egg-info/*",
+  "*/.next/*",
+  "*/ios/*",
+  "*/android/*",
+  "*/.expo/*",
+})
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- nicer diagnostic indicators
@@ -56,17 +68,17 @@ vim.diagnostic.config({
 
 -- hide all separators
 vim.opt.fillchars = {
-  vert = " ",      -- vertical separator
-  vertleft = " ",  -- for left separators
+  vert = " ",     -- vertical separator
+  vertleft = " ", -- for left separators
   vertright = " ", -- for right separators
-  horiz = " ",     -- horizontal separator
-  horizup = " ",   -- for top separators
+  horiz = " ",    -- horizontal separator
+  horizup = " ",  -- for top separators
   horizdown = " ", -- for bottom separators
-  eob = " ",       -- hide end-of-buffer tildes
+  eob = " ",      -- hide end-of-buffer tildes
 }
 --Keybinds should go under here
 --Tab keybinds
-vim.keymap.set("n", "<leader>tn", "<Cmd>tabnew<CR>", { silent = true, desc = "New Tab"})
+vim.keymap.set("n", "<leader>tn", "<Cmd>tabnew<CR>", { silent = true, desc = "New Tab" })
 vim.keymap.set("n", "<leader>tx", "<Cmd>tabclose<CR>", { silent = true, desc = "Close Tab" })
 
 for i = 1, 9 do
@@ -75,12 +87,52 @@ end
 vim.keymap.set("n", "<leader>0", "<Cmd>tablast<CR>", { desc = "Go to last tab" })
 
 --Copy and pasting
-vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', {desc = "Yank to system clipboard"})
-vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p', { noremap = true, silent = true, desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set(
+  { "n", "v", "x" },
+  "<leader>p",
+  '"+p',
+  { noremap = true, silent = true, desc = "Paste from system clipboard" }
+)
 
 -- Remove search highlights
-vim.keymap.set({ "n", "v", "x" }, "<leader>h", '<Cmd>noh<CR>', {desc = "Disable highlight for last search"})
+vim.keymap.set({ "n", "v", "x" }, "<leader>h", "<Cmd>noh<CR>", { desc = "Disable highlight for last search" })
 
 -- QOL quick exit and Save
 vim.keymap.set("n", "<leader>w", "<Cmd>w<CR>", { silent = true, desc = "Save" })
 vim.keymap.set("n", "<leader>q", "<Cmd>wq<CR>", { silent = true, desc = "Save and quit" })
+
+-- autocmds go under here
+-- ide like highlight when stopping cursor
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
+  desc = "Highlight references under cursor",
+  callback = function()
+    -- Only run if the cursor is not in insert mode
+    if vim.fn.mode() ~= "i" then
+      local clients = vim.lsp.get_clients({ bufnr = 0 })
+      local supports_highlight = false
+      for _, client in ipairs(clients) do
+        if client.server_capabilities.documentHighlightProvider then
+          supports_highlight = true
+          break -- Found a supporting client, no need to check others
+        end
+      end
+
+      --Proceed only if an LSP is active AND supports the feature
+      if supports_highlight then
+        vim.lsp.buf.clear_references()
+        vim.lsp.buf.document_highlight()
+      end
+    end
+  end,
+})
+
+-- ide like highlight when stopping cursor
+vim.api.nvim_create_autocmd("CursorMovedI", {
+  group = "LspReferenceHighlight",
+  desc = "Clear highlights when entering insert mode",
+  callback = function()
+    vim.lsp.buf.clear_references()
+  end,
+})
