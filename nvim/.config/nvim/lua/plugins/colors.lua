e08144ac4b62cbab02b3d2129bbb3db84cd6e92a
haxybaxy <zaidsaheb4@gmail.com>
2025-11-13 01:04:16 +0100

    add good theme switcher

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,58 +1,74 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("nightfox").setup({
-				options = {
-					styles = {
-						comments = "italic",
-						keywords = "bold",
-						types = "bold",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"uhs-robert/oasis.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("oasis").setup({
-				dark_style = "night",
-				light_style = "dawn",
-			})
-		end,
-	},
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.gruvbox_material_enable_bold = true
-			vim.g.gruvbox_material_enable_italic = true
-		end,
-	},
-	{
-		"sainnhe/everforest",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.everforest_background = "medium"
-			vim.g.everforest_enable_italic = true
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				style = "night",
-				light_style = "day",
-			})
-		end,
-	},
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nightfox").setup({
+        options = {
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "bold",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "uhs-robert/oasis.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("oasis").setup({
+        dark_style = "night",
+        light_style = "dawn",
+      })
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_bold = true
+      vim.g.gruvbox_material_enable_italic = true
+    end,
+  },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "medium"
+      vim.g.everforest_enable_italic = true
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        light_style = "day",
+      })
+    end,
+  },
+
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd.colorscheme("gruvbox-material")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd.colorscheme("dawnfox")
+      end,
+      update_interval = 3000,
+      fallback = "dark",
+    },
+  },
 }
