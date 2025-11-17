ab09ce997ce44f41d3fc3d7ec3ace2d06df502dc
haxybaxy <zaidsaheb4@gmail.com>
2025-11-18 02:42:33 +0300

    quicker settings

--- nvim/.config/nvim/lua/plugins/quicker.lua
+++ nvim/.config/nvim/lua/plugins/quicker.lua
@@ -4,6 +4,35 @@ return {
 	---@module "quicker"
 	opts = {},
 	config = function()
-		require("quicker").setup({})
+		require("quicker").setup({
+			vim.keymap.set("n", "<leader>q", function()
+				require("quicker").toggle()
+			end, {
+				desc = "Toggle quickfix",
+			}),
+			vim.keymap.set("n", "<leader>ll", function()
+				require("quicker").toggle({ loclist = true })
+			end, {
+				desc = "Toggle loclist",
+			}),
+			require("quicker").setup({
+				keys = {
+					{
+						">",
+						function()
+							require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+						end,
+						desc = "Expand quickfix context",
+					},
+					{
+						"<",
+						function()
+							require("quicker").collapse()
+						end,
+						desc = "Collapse quickfix context",
+					},
+				},
+			}),
+		})
 	end,
 }
