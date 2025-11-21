533389a22fb446e5bb264b23691693e28fb68317
haxybaxy <zaidsaheb4@gmail.com>
2025-11-21 14:29:28 +0300

    add cpp

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,39 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "json",
-        "python",
-        "javascript",
-        "typescript",
-        "tsx",
-        "html",
-        "css",
-        "scss",
-        "yaml",
-        "toml",
-        "markdown",
-        "markdown_inline",
-        "csv",
-        "graphql",
-        "dockerfile",
-        "gitignore",
-      },
-      highlight = { 
-        enable = true,
-        additional_vim_regex_highlighting = true,
-      },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			ensure_installed = {
+				"c",
+				"cpp",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"json",
+				"python",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"scss",
+				"yaml",
+				"toml",
+				"markdown",
+				"markdown_inline",
+				"csv",
+				"graphql",
+				"dockerfile",
+				"gitignore",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = true,
+			},
+			indent = { enable = true },
+		})
+	end,
 } -- syntax highlighting
