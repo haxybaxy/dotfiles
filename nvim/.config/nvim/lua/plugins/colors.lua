f6aadb76272d30baa03eca09c38bae54c0721162
haxybaxy <zaidsaheb4@gmail.com>
2025-11-25 16:22:11 +0300

    catpuccin

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -55,13 +55,13 @@ return {
       })
     end,
   },
-
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd.colorscheme("gruvbox-material")
+        vim.cmd.colorscheme("catppuccin-frappe")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
