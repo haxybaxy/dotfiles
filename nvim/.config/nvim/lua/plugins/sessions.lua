75cbed5b4b1fe750993cdcf340c685572502f255
haxybaxy <zaidsaheb4@gmail.com>
2025-12-06 14:37:29 +0300

    try without autosession for a bit

--- nvim/.config/nvim/lua/plugins/sessions.lua
+++ nvim/.config/nvim/lua/plugins/sessions.lua
@@ -1,14 +1,14 @@
 return {
-  { -- close unused buffers automatically
-    "chrisgrieser/nvim-early-retirement",
-    config = true,
-    event = "VeryLazy",
-  },
-  { -- auto-save and ressurect sessions
-    "rmagatti/auto-session",
-    lazy = false,
-    opts = {
-      suppressed_dirs = { "~/", "~/Downloads", "/" },
-    },
-  },
+	{ -- close unused buffers automatically
+		"chrisgrieser/nvim-early-retirement",
+		config = true,
+		event = "VeryLazy",
+	},
+	-- { -- auto-save and ressurect sessions
+	--   "rmagatti/auto-session",
+	--   lazy = false,
+	--   opts = {
+	--     suppressed_dirs = { "~/", "~/Downloads", "/" },
+	--   },
+	-- },
 }
