58c3a0e46c0ce6c972a5b3b7c33ca1bc7a84fb6d
haxybaxy <zaidsaheb4@gmail.com>
2025-12-09 17:27:42 +0300

    kanso

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,95 +1,95 @@
 return {
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          styles = {
-            comments = "italic",
-            keywords = "bold",
-            types = "bold",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    config = function()
-      require("onedarkpro").setup({
-        styles = {
-          types = "bold",
-          methods = "NONE",
-          numbers = "NONE",
-          strings = "NONE",
-          comments = "italic",
-          keywords = "bold",
-          constants = "NONE",
-          functions = "italic",
-          operators = "NONE",
-          variables = "NONE",
-          parameters = "NONE",
-          conditionals = "italic",
-          virtual_text = "NONE",
-        },
-        options = {
-          cursorline = true,
-        },
-      })
-    end,
-  },
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_enable_bold = true
-      vim.g.gruvbox_material_enable_italic = true
-    end,
-  },
-  {
-    "sainnhe/everforest",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.everforest_background = "medium"
-      vim.g.everforest_enable_italic = true
-    end,
-  },
-  {
-    "webhooked/kanso.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("kanso").setup({
-        bold = true,
-        italics = true,
-        compile = false,
-        undercurl = true,
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { bold = true },
-        statementStyle = {},
-        typeStyle = { bold = true },
-      })
-    end,
-  },
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd.colorscheme("nordfox")
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd.colorscheme("dawnfox")
-      end,
-      update_interval = 3000,
-      fallback = "dark",
-    },
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nightfox").setup({
+				options = {
+					styles = {
+						comments = "italic",
+						keywords = "bold",
+						types = "bold",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000,
+		config = function()
+			require("onedarkpro").setup({
+				styles = {
+					types = "bold",
+					methods = "NONE",
+					numbers = "NONE",
+					strings = "NONE",
+					comments = "italic",
+					keywords = "bold",
+					constants = "NONE",
+					functions = "italic",
+					operators = "NONE",
+					variables = "NONE",
+					parameters = "NONE",
+					conditionals = "italic",
+					virtual_text = "NONE",
+				},
+				options = {
+					cursorline = true,
+				},
+			})
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_enable_bold = true
+			vim.g.gruvbox_material_enable_italic = true
+		end,
+	},
+	{
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.everforest_background = "medium"
+			vim.g.everforest_enable_italic = true
+		end,
+	},
+	{
+		"webhooked/kanso.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("kanso").setup({
+				bold = true,
+				italics = true,
+				compile = false,
+				undercurl = true,
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { bold = true },
+				statementStyle = {},
+				typeStyle = { bold = true },
+			})
+		end,
+	},
+	{
+		"f-person/auto-dark-mode.nvim",
+		opts = {
+			set_dark_mode = function()
+				vim.api.nvim_set_option_value("background", "dark", {})
+				vim.cmd.colorscheme("kanso")
+			end,
+			set_light_mode = function()
+				vim.api.nvim_set_option_value("background", "light", {})
+				vim.cmd.colorscheme("dawnfox")
+			end,
+			update_interval = 3000,
+			fallback = "dark",
+		},
+	},
 }
