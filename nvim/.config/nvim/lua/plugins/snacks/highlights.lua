0719e13edb7b07f6c26f26b30b56fef5d8b47311
haxybaxy <zaidsaheb4@gmail.com>
2025-12-09 23:13:10 +0300

    tokyonight i will never betray you again

--- nvim/.config/nvim/lua/plugins/snacks/highlights.lua
+++ nvim/.config/nvim/lua/plugins/snacks/highlights.lua
@@ -1,38 +1,87 @@
 local M = {}
 
--- Define highlight mappings here
-M.highlight_map = {
-	SnacksPicker = "Pmenu",
-	SnacksPickerListCursorLine = "PmenuSel",
-	SnacksPickerInputCursorLine = "PmenuSel",
-	SnacksPickerBoxCursorLine = "PmenuSel",
-	SnacksPickerPreviewCursorLine = "PmenuSel",
-	SnacksPickerPickWin = "Pmenu",
-	SnacksPickerPickWinCurrent = "Pmenu",
-	SnacksPickerBorder = "Pmenu",
-	SnacksPickerTree = "SnacksPickerUnselected",
-	SnacksPickerCol = "Pmenu",
-	SnacksPickerPrompt = "Pmenu",
-	SnacksInputTitle = "SnacksPickerList",
+---------------------------------------------------------------------
+-- Highlight group definitions
+---------------------------------------------------------------------
+
+-- Simple link targets
+M.link_map = {
+  -- Snacks
+  SnacksPicker = "Pmenu",
+  SnacksPickerListCursorLine = "PmenuSel",
+  SnacksPickerInputCursorLine = "PmenuSel",
+  SnacksPickerBoxCursorLine = "PmenuSel",
+  SnacksPickerPreviewCursorLine = "PmenuSel",
+  SnacksPickerPickWin = "Pmenu",
+  SnacksPickerPickWinCurrent = "Pmenu",
+  SnacksPickerBorder = "Pmenu",
+  SnacksPickerTree = "SnacksPickerUnselected",
+  SnacksPickerCol = "Pmenu",
+  SnacksPickerPrompt = "Pmenu",
+  SnacksInputTitle = "SnacksPickerList",
 }
 
--- Apply custom highlight groups for Snacks components
-function M.apply_highlights()
-	for hl_group, link_target in pairs(M.highlight_map) do
-		vim.api.nvim_set_hl(0, hl_group, { link = link_target })
-	end
+-- Style groups (not links)
+M.style_map = {
+  -- LSP refs
+  LspReferenceText = { underline = true },
+  LspReferenceRead = { underline = true },
+  LspReferenceWrite = { underline = true },
 
-	vim.api.nvim_set_hl(0, "LspReferenceText", { underline = true })
-	vim.api.nvim_set_hl(0, "LspReferenceRead", { underline = true })
-	vim.api.nvim_set_hl(0, "LspReferenceWrite", { underline = true })
+  -----------------------------------------------------------------
+  -- Universal keyword styles
+  -----------------------------------------------------------------
+  Keyword = { bold = true },
+  Conditional = { bold = true },
+  Repeat = { bold = true },
+  Label = { bold = true },
+  Operator = { bold = true },
+  ["@keyword"] = { bold = true },
+  ["@keyword.return"] = { bold = true },
+  ["@keyword.operator"] = { bold = true },
+  ["@keyword.function"] = { bold = true },
+  ["@conditional"] = { bold = true },
+  ["@repeat"] = { bold = true },
+  ["@operator"] = { bold = true },
+
+  -----------------------------------------------------------------
+  -- Universal type styles
+  -----------------------------------------------------------------
+  Type = { bold = true },
+  StorageClass = { bold = true },
+  Structure = { bold = true },
+  Typedef = { bold = true },
+  ["@type"] = { bold = true },
+  ["@type.builtin"] = { bold = true },
+  ["@type.definition"] = { bold = true },
+  ["@type.qualifier"] = { bold = true },
+}
+
+---------------------------------------------------------------------
+-- Apply highlights
+---------------------------------------------------------------------
+function M.apply_highlights()
+  local set = vim.api.nvim_set_hl
+
+  -- Apply linked groups
+  for group, link_target in pairs(M.link_map) do
+    set(0, group, { link = link_target })
+  end
+
+  -- Apply style groups
+  for group, opts in pairs(M.style_map) do
+    set(0, group, opts)
+  end
 end
 
--- Set up autocommand to apply highlights after colorscheme changes
+---------------------------------------------------------------------
+-- Autocmd hook
+---------------------------------------------------------------------
 function M.setup_highlight_autocmd()
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		group = vim.api.nvim_create_augroup("SnacksHighlights", { clear = true }),
-		callback = M.apply_highlights,
-	})
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    group = vim.api.nvim_create_augroup("SnacksHighlights", { clear = true }),
+    callback = M.apply_highlights,
+  })
 end
 
 return M
