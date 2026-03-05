4198166af59c993101f84d4d9d26603e9817e2e1
haxybaxy <zaidsaheb4@gmail.com>
2026-03-05 18:21:57 +0100

    lint

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -15,7 +15,16 @@ return {
 			})
 		end,
 	},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+		end,
+	},
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
