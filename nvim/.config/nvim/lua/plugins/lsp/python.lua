db7bad5eaf4c35612daa7b035905d17de19a5749
haxybaxy <zaidsaheb4@gmail.com>
2026-03-31 22:27:57 +0200

    disable inlay hints by default in python

--- nvim/.config/nvim/lua/plugins/lsp/python.lua
+++ nvim/.config/nvim/lua/plugins/lsp/python.lua
@@ -1,42 +1,41 @@
 local M = {}
 
 M.setup = function(lspconfig, capabilities)
-	-- Ruff LSP for linting
-	lspconfig.ruff.setup({
-		capabilities = capabilities,
-	})
+  -- Ruff LSP for linting
+  lspconfig.ruff.setup({
+    capabilities = capabilities,
+  })
 
-	-- Basedpyright for type checking and completions
-	lspconfig.basedpyright.setup({
-		capabilities = capabilities,
-		settings = {
-			basedpyright = {
-				analysis = {
-					typeCheckingMode = "strict", -- or "standard" or "strict"
-					autoSearchPaths = true,
-					useLibraryCodeForTypes = true,
-					diagnosticMode = "workspace",
-				},
-			},
-			python = {
-				analysis = {
-					inlayHints = {
-						variableTypes = true,
-						functionReturnTypes = true,
-						callArgumentNames = true,
-						pytestParameters = true,
-					},
-				},
-			},
-		},
-		on_attach = function(client, bufnr)
-			-- Enable inlay hints for this buffer
-			if client.server_capabilities.inlayHintProvider then
-				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-			end
-		end,
-	})
+  -- Basedpyright for type checking and completions
+  lspconfig.basedpyright.setup({
+    capabilities = capabilities,
+    settings = {
+      basedpyright = {
+        analysis = {
+          typeCheckingMode = "strict", -- or "standard" or "strict"
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = true,
+          diagnosticMode = "workspace",
+        },
+      },
+      python = {
+        analysis = {
+          inlayHints = {
+            variableTypes = true,
+            functionReturnTypes = true,
+            callArgumentNames = true,
+            pytestParameters = true,
+          },
+        },
+      },
+    },
+    on_attach = function(client, bufnr)
+      -- Enable inlay hints for this buffer
+      if client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
+      end
+    end,
+  })
 end
 
 return M
-
