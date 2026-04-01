ab5114c90a21a7892e617072a91eb7e019345f43
haxybaxy <zaidsaheb4@gmail.com>
2026-04-02 01:46:49 +0200

    remove bloat

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,39 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+
+			python = function(bufnr)
+				if require("conform").get_formatter_info("ruff_format", bufnr).available then
+					return { "ruff_format" }
+				else
+					return { "isort", "black" }
+				end
+			end,
+
+			["*"] = { "codespell" },
+			["_"] = { "trim_whitespace" },
+		},
+
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+		},
+
+		log_level = vim.log.levels.ERROR,
+
+		notify_on_error = true,
+		notify_no_formatters = true,
+	},
+}
