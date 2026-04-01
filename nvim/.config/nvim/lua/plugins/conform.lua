04487df20bbbe38383fad39f0587f7b65d0d4af2
haxybaxy <zaidsaheb4@gmail.com>
2026-04-02 01:47:51 +0200

    add format keybind

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,14 @@
 return {
 	"stevearc/conform.nvim",
+	keys = {
+		{
+			"<leader>gf",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			desc = "Format buffer",
+		},
+	},
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
