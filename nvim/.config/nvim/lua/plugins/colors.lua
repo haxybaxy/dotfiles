29250a2e509fa211bc7f9c352f733b5ee1d3b643
haxybaxy <zaidsaheb4@gmail.com>
2026-04-02 02:10:57 +0200

    switch to blink

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -3,6 +3,12 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		require("catppuccin").setup({
+			float = {
+				transparent = false,-- enable transparent floating windows
+				solid = true, -- use solid styling for floating windows, see |winborder|
+			},
+		})
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -34,7 +34,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
       local lspconfig = require("lspconfig")
 
       -- Load language-specific LSP configurations
