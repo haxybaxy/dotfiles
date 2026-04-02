30f76437e29c681ccf1fc5cad17b6d9cddae2d08
haxybaxy <zaidsaheb4@gmail.com>
2026-04-02 16:33:09 +0200

    schema store and markdown preview

--- nvim/.config/nvim/lsp/jsonls.lua
+++ nvim/.config/nvim/lsp/jsonls.lua
@@ -4,4 +4,10 @@ return {
     cmd = { 'vscode-json-language-server', '--stdio' },
     filetypes = { 'json', 'jsonc' },
     root_markers = { 'package.json', '.git' },
+    settings = {
+        json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enable = true },
+        },
+    },
 }
---
+++ nvim/.config/nvim/lsp/yamlls.lua
@@ -0,0 +1,16 @@
+-- npm install -g yaml-language-server
+---@type vim.lsp.Config
+return {
+    cmd = { 'yaml-language-server', '--stdio' },
+    filetypes = { 'yaml', 'yaml.docker-compose' },
+    root_markers = { '.git' },
+    settings = {
+        yaml = {
+            schemaStore = {
+                enable = false,
+                url = '',
+            },
+            schemas = require('schemastore').yaml.schemas(),
+        },
+    },
+}
