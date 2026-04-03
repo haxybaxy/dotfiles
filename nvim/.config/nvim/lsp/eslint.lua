cc6c72e3cd9c67ac46153a522d5b897b0ed4f0ed
haxybaxy <zaidsaheb4@gmail.com>
2026-04-04 01:29:15 +0200

    lint

--- nvim/.config/nvim/lsp/eslint.lua
+++ nvim/.config/nvim/lsp/eslint.lua
@@ -1,54 +1,62 @@
 -- npm install -g vscode-langservers-extracted
 ---@type vim.lsp.Config
 return {
-    cmd = { 'vscode-eslint-language-server', '--stdio' },
-    filetypes = {
-        'javascript', 'javascriptreact',
-        'typescript', 'typescriptreact',
-    },
-    root_markers = {
-        'eslint.config.js', 'eslint.config.mjs', 'eslint.config.cjs',
-        '.eslintrc', '.eslintrc.js', '.eslintrc.json', '.eslintrc.yml',
-        'package.json', '.git',
-    },
-    settings = {
-        validate = 'on',
-        packageManager = vim.NIL,
-        useESLintClass = false,
-        experimental = { useFlatConfig = false },
-        codeActionOnSave = { enable = false, mode = 'all' },
-        format = false,
-        quiet = false,
-        onIgnoredFiles = 'off',
-        options = {},
-        rulesCustomizations = {},
-        run = 'onType',
-        problems = { shortenToSingleLine = false },
-        nodePath = '',
-        workingDirectory = { mode = 'location' },
-        codeAction = {
-            disableRuleComment = { enable = true, location = 'separateLine' },
-            showDocumentation = { enable = true },
-        },
-    },
-    before_init = function(params, config)
-        config.settings.workspaceFolder = {
-            uri = params.rootPath,
-            name = vim.fn.fnamemodify(params.rootPath, ':t'),
-        }
-    end,
-    handlers = {
-        ['eslint/openDoc'] = function(_, params)
-            vim.ui.open(params.url)
-            return {}
-        end,
-        ['eslint/probeFailed'] = function()
-            vim.notify('LSP[eslint]: Probe failed.', vim.log.levels.WARN)
-            return {}
-        end,
-        ['eslint/noLibrary'] = function()
-            vim.notify('LSP[eslint]: Unable to load ESLint library.', vim.log.levels.WARN)
-            return {}
-        end,
-    },
+	cmd = { "vscode-eslint-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+	},
+	root_markers = {
+		"eslint.config.js",
+		"eslint.config.mjs",
+		"eslint.config.cjs",
+		".eslintrc",
+		".eslintrc.js",
+		".eslintrc.json",
+		".eslintrc.yml",
+		"package.json",
+		".git",
+	},
+	settings = {
+		validate = "on",
+		packageManager = vim.NIL,
+		useESLintClass = false,
+		experimental = { useFlatConfig = false },
+		codeActionOnSave = { enable = false, mode = "all" },
+		format = false,
+		quiet = false,
+		onIgnoredFiles = "off",
+		options = {},
+		rulesCustomizations = {},
+		run = "onType",
+		problems = { shortenToSingleLine = false },
+		nodePath = "",
+		workingDirectory = { mode = "location" },
+		codeAction = {
+			disableRuleComment = { enable = true, location = "separateLine" },
+			showDocumentation = { enable = true },
+		},
+	},
+	before_init = function(params, config)
+		config.settings.workspaceFolder = {
+			uri = params.rootPath,
+			name = vim.fn.fnamemodify(params.rootPath, ":t"),
+		}
+	end,
+	handlers = {
+		["eslint/openDoc"] = function(_, params)
+			vim.ui.open(params.url)
+			return {}
+		end,
+		["eslint/probeFailed"] = function()
+			vim.notify("LSP[eslint]: Probe failed.", vim.log.levels.WARN)
+			return {}
+		end,
+		["eslint/noLibrary"] = function()
+			vim.notify("LSP[eslint]: Unable to load ESLint library.", vim.log.levels.WARN)
+			return {}
+		end,
+	},
 }
--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -2,53 +2,57 @@ vim.pack.add({ "https://github.com/b0o/SchemaStore.nvim" })
 
 -- Diagnostics configuration
 vim.diagnostic.config({
-    virtual_text = true,
-    float = { source = true },
-    severity_sort = true,
-    signs = true,
+	virtual_text = true,
+	float = { source = true },
+	severity_sort = true,
+	signs = true,
 })
 
 -- LSP keymaps on attach
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        local bufnr = args.buf
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if not client then return end
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then
+			return
+		end
 
-        local map = function(mode, lhs, rhs, desc)
-            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-        end
+		local map = function(mode, lhs, rhs, desc)
+			vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+		end
 
-        map('n', 'K', vim.lsp.buf.hover, 'Hover info')
-        map('n', '<leader>ca', vim.lsp.buf.code_action, 'Code action')
-        map('n', '<leader>gn', vim.lsp.buf.rename, 'Rename symbol')
-        map('n', '<leader>D', vim.lsp.buf.type_definition, 'Type definition')
-        map('n', '<leader>ds', vim.diagnostic.open_float, 'Show diagnostics')
-        map('n', '<leader>ih', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-        end, 'Toggle inlay hints')
+		map("n", "K", vim.lsp.buf.hover, "Hover info")
+		map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+		map("n", "<leader>gn", vim.lsp.buf.rename, "Rename symbol")
+		map("n", "<leader>D", vim.lsp.buf.type_definition, "Type definition")
+		map("n", "<leader>ds", vim.diagnostic.open_float, "Show diagnostics")
+		map("n", "<leader>ih", function()
+			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+		end, "Toggle inlay hints")
 
-        -- Document highlighting
-        if client:supports_method('textDocument/documentHighlight') then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                buffer = bufnr,
-                callback = vim.lsp.buf.document_highlight,
-            })
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                buffer = bufnr,
-                callback = vim.lsp.buf.clear_references,
-            })
-        end
-    end,
+		-- Document highlighting
+		if client:supports_method("textDocument/documentHighlight") then
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = bufnr,
+				callback = vim.lsp.buf.document_highlight,
+			})
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = bufnr,
+				callback = vim.lsp.buf.clear_references,
+			})
+		end
+	end,
 })
 
 -- Inject blink.cmp capabilities into all servers
-vim.lsp.config('*', {
-    capabilities = require('blink.cmp').get_lsp_capabilities(),
+vim.lsp.config("*", {
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
 
 -- Discover and enable all servers from lsp/ directory
-local servers = vim.iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
-    :map(function(file) return vim.fn.fnamemodify(file, ':t:r') end)
-    :totable()
+local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+	:map(function(file)
+		return vim.fn.fnamemodify(file, ":t:r")
+	end)
+	:totable()
 vim.lsp.enable(servers)
