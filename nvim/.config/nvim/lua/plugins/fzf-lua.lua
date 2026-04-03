74ab898a4ee87eed27343585e6cc234cab31b970
haxybaxy <zaidsaheb4@gmail.com>
2026-04-03 23:34:30 +0200

    lint

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -14,13 +14,61 @@ return {
 	},
 	---@diagnostic enable: missing-fields
 	keys = {
-		{ "<leader><space>", function() require("fzf-lua").live_grep() end, desc = "Ripgrep" },
-		{ "<leader>f", function() require("fzf-lua").files() end, desc = "Find Files" },
-		{ "<leader>x", function() require("fzf-lua").diagnostics_workspace() end, desc = "Workspace Diagnostics" },
-		{ "<leader>r", function() require("fzf-lua").oldfiles() end, desc = "Recent" },
-		{ "<leader>gd", function() require("fzf-lua").lsp_definitions() end, desc = "LSP Definitions" },
-		{ "<leader>gr", function() require("fzf-lua").lsp_references() end, desc = "LSP References" },
-		{ "<leader>bf", function() require("fzf-lua").buffers() end, desc = "Buffers" },
-		{ "<leader>sH", function() require("fzf-lua").highlights() end, desc = "Highlights" },
+		{
+			"<leader><space>",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "Ripgrep",
+		},
+		{
+			"<leader>f",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>x",
+			function()
+				require("fzf-lua").diagnostics_workspace()
+			end,
+			desc = "Workspace Diagnostics",
+		},
+		{
+			"<leader>r",
+			function()
+				require("fzf-lua").oldfiles()
+			end,
+			desc = "Recent",
+		},
+		{
+			"<leader>gd",
+			function()
+				require("fzf-lua").lsp_definitions()
+			end,
+			desc = "LSP Definitions",
+		},
+		{
+			"<leader>gr",
+			function()
+				require("fzf-lua").lsp_references()
+			end,
+			desc = "LSP References",
+		},
+		{
+			"<leader>bf",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>sH",
+			function()
+				require("fzf-lua").highlights()
+			end,
+			desc = "Highlights",
+		},
 	},
 }
