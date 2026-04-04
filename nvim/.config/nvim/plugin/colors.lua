64db9d8d2c4d2176b798506b9154ef7aac1d762b
haxybaxy <zaidsaheb4@gmail.com>
2026-04-04 17:07:20 +0200

    add evergarden for eye health

--- nvim/.config/nvim/plugin/colors.lua
+++ nvim/.config/nvim/plugin/colors.lua
@@ -1,2 +1,22 @@
 vim.pack.add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.pack.add({ { src = "https://codeberg.org/evergarden/nvim.git", name = "evergarden" } })
+
+require("evergarden").setup({
+	theme = {
+		variant = "spring", -- 'winter'|'fall'|'spring'|'summer'
+		accent = "aqua",
+	},
+	editor = {
+		transparent_background = false,
+		sign = { color = "none" },
+		float = {
+			color = "mantle",
+			solid_border = false,
+		},
+		completion = {
+			color = "surface0",
+		},
+	},
+})
+
+vim.cmd.colorscheme("evergarden")
