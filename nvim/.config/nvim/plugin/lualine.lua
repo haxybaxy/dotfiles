afba80f74f730adb02c075844371d99d9b33dac7
haxybaxy <zaidsaheb4@gmail.com>
2026-04-06 01:03:32 +0200

    nice comment

--- nvim/.config/nvim/plugin/lualine.lua
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -1,99 +1,99 @@
 vim.pack.add({
-  "https://github.com/nvim-lualine/lualine.nvim",
-  "https://github.com/nvim-tree/nvim-web-devicons",
-  "https://github.com/folke/noice.nvim",
-  "https://github.com/MunifTanjim/nui.nvim",
-  "https://github.com/rcarriga/nvim-notify",
+	"https://github.com/nvim-lualine/lualine.nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/folke/noice.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	"https://github.com/rcarriga/nvim-notify",
 })
-
+-- helix looking bar
 local function mode_text()
-  local mode = vim.fn.mode()
-  local mode_name = {
-    n = "NOR",
-    i = "INS",
-    v = "VIS",
-    V = "V-LINE",
-    [""] = "V-BLOCK",
-    c = "COM",
-    R = "REPLACE",
-  }
-  return mode_name[mode] or mode
+	local mode = vim.fn.mode()
+	local mode_name = {
+		n = "NOR",
+		i = "INS",
+		v = "VIS",
+		V = "V-LINE",
+		[""] = "V-BLOCK",
+		c = "COM",
+		R = "REPLACE",
+	}
+	return mode_name[mode] or mode
 end
 
 require("lualine").setup({
-  options = {
-    theme = {
-      normal = {
-        a = { bg = "none" },
-        b = { bg = "none" },
-        c = { bg = "none" },
-      },
-      insert = {
-        a = { bg = "none" },
-        b = { bg = "none" },
-        c = { bg = "none" },
-      },
-      visual = {
-        a = { bg = "none" },
-        b = { bg = "none" },
-        c = { bg = "none" },
-      },
-      replace = {
-        a = { bg = "none" },
-        b = { bg = "none" },
-        c = { bg = "none" },
-      },
-      command = {
-        a = { bg = "none" },
-        b = { bg = "none" },
-        c = { bg = "none" },
-      },
-      inactive = {
-        a = { fg = "#888888", bg = "none" },
-        b = { fg = "#888888", bg = "none" },
-        c = { fg = "#888888", bg = "none" },
-      },
-    },
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    always_divide_middle = false,
-  },
-  sections = {
-    lualine_a = {
-      {
-        mode_text,
-        color = function()
-          return { bg = "none" }
-        end,
-      },
-    },
-    lualine_b = { { "filename" } },
-    lualine_c = {},
-    lualine_x = {
-      {
-        require("noice").api.status.mode.get,
-        cond = require("noice").api.status.mode.has,
-      },
-    },
-    lualine_y = {
-      {
-        "diagnostics",
-        sources = { "nvim_diagnostic" },
-        symbols = {
-          error = "● ",
-          warn = "● ",
-          info = "● ",
-          hint = "● ",
-        },
-        diagnostics_color = {
-          error = { fg = "#e06c75" },
-          warn = { fg = "#e5c07b" },
-          info = { fg = "#61afef" },
-          hint = { fg = "#56b6c2" },
-        },
-      },
-      "branch",
-    },
-    lualine_z = { "location" },
-  },
+	options = {
+		theme = {
+			normal = {
+				a = { bg = "none" },
+				b = { bg = "none" },
+				c = { bg = "none" },
+			},
+			insert = {
+				a = { bg = "none" },
+				b = { bg = "none" },
+				c = { bg = "none" },
+			},
+			visual = {
+				a = { bg = "none" },
+				b = { bg = "none" },
+				c = { bg = "none" },
+			},
+			replace = {
+				a = { bg = "none" },
+				b = { bg = "none" },
+				c = { bg = "none" },
+			},
+			command = {
+				a = { bg = "none" },
+				b = { bg = "none" },
+				c = { bg = "none" },
+			},
+			inactive = {
+				a = { fg = "#888888", bg = "none" },
+				b = { fg = "#888888", bg = "none" },
+				c = { fg = "#888888", bg = "none" },
+			},
+		},
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		always_divide_middle = false,
+	},
+	sections = {
+		lualine_a = {
+			{
+				mode_text,
+				color = function()
+					return { bg = "none" }
+				end,
+			},
+		},
+		lualine_b = { { "filename" } },
+		lualine_c = {},
+		lualine_x = {
+			{
+				require("noice").api.status.mode.get,
+				cond = require("noice").api.status.mode.has,
+			},
+		},
+		lualine_y = {
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = {
+					error = "● ",
+					warn = "● ",
+					info = "● ",
+					hint = "● ",
+				},
+				diagnostics_color = {
+					error = { fg = "#e06c75" },
+					warn = { fg = "#e5c07b" },
+					info = { fg = "#61afef" },
+					hint = { fg = "#56b6c2" },
+				},
+			},
+			"branch",
+		},
+		lualine_z = { "location" },
+	},
 })
