6a63d02fca1f47d76b145110a295c450cb05e51d
haxybaxy <zaidsaheb4@gmail.com>
2026-04-06 01:00:59 +0200

    remove unused keymap

--- nvim/.config/nvim/plugin/flash.lua
+++ nvim/.config/nvim/plugin/flash.lua
@@ -3,9 +3,5 @@ vim.pack.add({ "https://github.com/folke/flash.nvim" })
 require("flash").setup()
 
 vim.keymap.set({ "n", "x", "o" }, "<leader>m", function()
-  require("flash").jump()
+	require("flash").jump()
 end, { desc = "Flash" })
-
-vim.keymap.set({ "c" }, "<c-s>", function()
-  require("flash").toggle()
-end, { desc = "Toggle Flash Search" })
