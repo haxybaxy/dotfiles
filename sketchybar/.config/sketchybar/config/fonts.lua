9e05837323c1e6fe8062ff889339e2641c8d2ea7
haxybaxy <zaidsaheb4@gmail.com>
2026-06-02 22:18:37 +0200

    revert

--- sketchybar/.config/sketchybar/config/fonts.lua
+++ sketchybar/.config/sketchybar/config/fonts.lua
@@ -1,8 +1,8 @@
 local dimens = require("config.dimens")
 
 return {
-	text = "MesloLGM Nerd Font",
-	numbers = "MesloLGM Nerd Font",
+	text = "JetBrainsMono Nerd Font",
+	numbers = "JetBrainsMono Nerd Font",
 	icons = function(size)
 		local font = "sketchybar-app-font:Regular"
 		return size and font .. ":" .. size or font .. ":" .. dimens.text.icon
