e0aa66b156b37b3636106c284ab39d0434ce7151
haxybaxy <zaidsaheb4@gmail.com>
2026-06-02 21:54:11 +0200

    meslolgl

--- sketchybar/.config/sketchybar/config/fonts.lua
+++ sketchybar/.config/sketchybar/config/fonts.lua
@@ -1,14 +1,14 @@
 local dimens = require("config.dimens")
 
 return {
-  text = "JetBrainsMono Nerd Font",
-  numbers = "JetBrainsMono Nerd Font",
-  icons = function(size)
-    local font = "sketchybar-app-font:Regular"
-    return size and font .. ":" .. size or font .. ":" .. dimens.text.icon
-  end,
-  styles = {
-    regular = "Regular",
-    bold = "Bold",
-  },
+	text = "MesloLGL Nerd Font",
+	numbers = "MesloLGL Nerd Font",
+	icons = function(size)
+		local font = "sketchybar-app-font:Regular"
+		return size and font .. ":" .. size or font .. ":" .. dimens.text.icon
+	end,
+	styles = {
+		regular = "Regular",
+		bold = "Bold",
+	},
 }
