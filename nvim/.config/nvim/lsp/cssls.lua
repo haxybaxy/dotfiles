9f8e4634bbda5a399777e7cab4bf0339274ffc22
haxybaxy <zaidsaheb4@gmail.com>
2026-07-17 01:32:30 +0200

    add css settings

--- nvim/.config/nvim/lsp/cssls.lua
+++ nvim/.config/nvim/lsp/cssls.lua
@@ -4,4 +4,11 @@ return {
     cmd = { 'vscode-css-language-server', '--stdio' },
     filetypes = { 'css', 'scss', 'less' },
     root_markers = { 'package.json', '.git' },
+    init_options = { provideFormatter = true },
+    -- the server skips validation entirely unless these are set
+    settings = {
+        css = { validate = true },
+        scss = { validate = true },
+        less = { validate = true },
+    },
 }
